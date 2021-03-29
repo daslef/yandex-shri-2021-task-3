72053c5c83ccb1a57d00848a3bc31ab7f1b1bec8
lex <daslef93@gmail.com>
2021-03-29 22:09:40 +0300

    webpack: separate configs for app (task3) and slides(task1)

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,84 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+
+const sharedConfig = {
     mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader, 
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    'css-loader',
+                    'resolve-url-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                          postcssOptions: {
+                            plugins: [
+                              [
+                                "postcss-preset-env",
+                                { browsers: 'last 2 versions' },
+                                "autoprefixer",
+                              ],
+                            ],
+                          },
+                        },
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(ttf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      limit: 8192,
+                    }
+                  },
+                ],
+               type: 'javascript/auto'
+              },
+        ],
+    },
+}
+
+const buildApp = {
+    name: 'buildApp',
     entry: {
         index: './src/index.ts',
         frame: './src/frame.ts',
@@ -13,25 +89,11 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: { contentBase: './dist' },
     plugins: [
-        new CleanWebpackPlugin(), 
         new CopyPlugin({ patterns: ['public'] }),
         new HtmlWebpackPlugin({ filename: 'index.html', chunks: ['index'], template: 'src/index.html' }),
         new HtmlWebpackPlugin({ filename: 'frame.html', chunks: ['frame'], template: 'src/frame.html' }),
         new MiniCssExtractPlugin(),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-        ],
-    },
     resolve: {
         extensions: ['.ts', '.js'],
     },
@@ -43,3 +105,21 @@ module.exports = {
         hints: false,
     },
 };
+  
+const buildSlides = {
+    name: 'buildSlides',
+    entry: './src/slides/stories.js',
+    output: {
+      filename: 'stories.js',
+      path: path.resolve(__dirname, 'public')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+           filename: 'stories.css',
+        }),
+    ]
+};
+  
+  
+  
+module.exports = [{ ...sharedConfig, ...buildApp }, { ...sharedConfig, ...buildSlides }];
